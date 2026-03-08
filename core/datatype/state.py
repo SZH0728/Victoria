0dc5bf3c0cede2833672ee43219d9bd7aa39ae30
Sun0728 <15064807+sun0728@user.noreply.gitee.com>
2026-03-08 10:49:20 +0800

    refactor(datatype): 修复州文件数据类字段命名错误并改进文档

--- core/datatype/state.py
+++ core/datatype/state.py
@@ -1,11 +1,6 @@
 # -*- coding:utf-8 -*-
 # AUTHOR: Sun
 
-"""
-@brief 州数据定义模块
-@details 定义了维多利亚3游戏中州相关的数据结构，包括州与国家的关系、文化信息等
-"""
-
 from dataclasses import dataclass
 
 from core.datatype.prefix import StateNamePrefix, CountryTagPrefix, CultureNamePrefix
@@ -14,15 +9,17 @@ from core.datatype.prefix import StateNamePrefix, CountryTagPrefix, CultureNameP
 @dataclass(frozen=True)
 class StateCountryItem(object):
     """
-    @brief 州-国家项数据类
-    @details 表示一个州与国家之间的关系，包含国家标签、所属省份和州类型信息
+    @brief 州国家项目数据类
+    @details 表示一个国家在州中的所有权信息，包含国家、拥有的省份和州类型
     """
-    country: CountryTagPrefix           #!< 国家标签
-    owned_provinces: tuple[str, ...]    #!< 所属省份ID列表
-    state_type: str | None              #!< 州类型
+    country: CountryTagPrefix             # 国家标签
+    owned_provinces: tuple[str, ...]      # 拥有的省份列表
+    state_type: str | None                # 州类型
 
     def __post_init__(self):
-        """@brief 初始化后处理，确保owned_provinces为元组类型"""
+        """
+        @brief 后初始化方法，确保列表类型属性转换为元组
+        """
         if isinstance(self.owned_provinces, list):
             object.__setattr__(self, 'owned_provinces', tuple(self.owned_provinces))
 
@@ -30,15 +27,17 @@ class StateCountryItem(object):
 @dataclass(frozen=True)
 class StateItem(object):
     """
-    @brief 州数据项
-    @details 表示一个州的所有相关信息，包括国家所有权、本土文化和宣称文化
+    @brief 州项目数据类
+    @details 表示游戏中的一个州，包含创建州信息、家园文化和宣称国家
     """
-    create_state: tuple[StateCountryItem, ...]    #!< 国家所有权列表（一个州可能有多个国家拥有）
-    add_homeland: tuple[CultureNamePrefix, ...]   #!< 本土文化列表
-    add_claim: tuple[CountryTagPrefix, ...]       #!< 宣称文化列表
+    create_state: tuple[StateCountryItem, ...]   # 创建州信息列表
+    add_homeland: tuple[CultureNamePrefix, ...]  # 家园文化列表
+    add_claim: tuple[CountryTagPrefix, ...]      # 宣称国家列表
 
     def __post_init__(self):
-        """@brief 初始化后处理，确保所有字段都是元组类型"""
+        """
+        @brief 后初始化方法，确保列表类型属性转换为元组
+        """
         if isinstance(self.create_state, list):
             object.__setattr__(self, 'create_state', tuple(self.create_state))
         if isinstance(self.add_homeland, list):
@@ -50,11 +49,11 @@ class StateItem(object):
 @dataclass(frozen=True)
 class StateFile(object):
     """
-    @brief 州文件数据容器
-    @details 包含州数据的根键和州项字典
+    @brief 州文件数据类
+    @details 表示整个州文件的数据结构，包含根键和州项目字典
     """
-    root_key: str | None
-    stat_item_dict: dict[StateNamePrefix, StateItem]
+    root_key: str | None                               # 根键
+    state_item_dict: dict[StateNamePrefix, StateItem]   # 州项目字典
 
 
 if __name__ == '__main__':
