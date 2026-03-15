f86f1225e20783fc4ca17b05442c60c43a92b57f
Sun0728 <15064807+sun0728@user.noreply.gitee.com>
2026-03-15 13:09:54 +0800

    feat(datatype): 添加结构化国家效果数据类

---
+++ core/datatype/structure/effect.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# AUTHOR: Sun
+
+from core.datatype.source.effect import EffectCountry
+
+
+StructuredEffectCountry = EffectCountry
+
+
+if __name__ == '__main__':
+    pass
