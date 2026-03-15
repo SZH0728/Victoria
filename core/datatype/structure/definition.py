f79be1fe45e0831c3d8ea8c08d84f43f42f5acbd
Sun0728 <15064807+sun0728@user.noreply.gitee.com>
2026-03-15 13:58:16 +0800

    feat(datatype): 完善结构化国家定义数据类定义

--- core/datatype/structure/definition.py
+++ core/datatype/structure/definition.py
@@ -1,9 +1,20 @@
 # -*- coding:utf-8 -*-
 # AUTHOR: Sun
 
+"""
+@brief 结构化国家定义数据模块
+@details 提供国家定义数据类的结构化版本别名
+"""
+
 from core.datatype.source.definition import DefinitionCountry
 
+
 StructuredDefinitionCountry = DefinitionCountry
+"""
+@brief 结构化国家定义数据类
+@details 国家定义数据的结构化版本别名，与原始 DefinitionCountry 相同
+"""
+
 
 if __name__ == '__main__':
     pass
