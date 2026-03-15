00f164377c0449b8f6cabedfc3749d24d27e95b1
Sun0728 <15064807+sun0728@user.noreply.gitee.com>
2026-03-15 13:57:44 +0800

    feat(datatype): 完善结构化国家效果数据类定义

--- core/datatype/structure/effect.py
+++ core/datatype/structure/effect.py
@@ -1,10 +1,19 @@
 # -*- coding:utf-8 -*-
 # AUTHOR: Sun
 
+"""
+@brief 结构化国家效果数据模块
+@details 提供国家效果数据类的结构化版本别名
+"""
+
 from core.datatype.source.effect import EffectCountry
 
 
 StructuredEffectCountry = EffectCountry
+"""
+@brief 结构化国家效果数据类
+@details 国家效果数据的结构化版本别名，与原始 EffectCountry 相同
+"""
 
 
 if __name__ == '__main__':
