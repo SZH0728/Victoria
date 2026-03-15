c9d4b5d2ecc12bc75cc3528a1f234533c1bbbc17
Sun0728 <15064807+sun0728@user.noreply.gitee.com>
2026-03-15 13:58:30 +0800

    feat(datatype): 完善结构化区域数据类定义

--- core/datatype/structure/region.py
+++ core/datatype/structure/region.py
@@ -1,9 +1,19 @@
 # -*- coding:utf-8 -*-
 # AUTHOR: Sun
 
+"""
+@brief 结构化区域数据模块
+@details 提供区域数据类的结构化版本别名
+"""
+
 from core.datatype.source.region import RegionItem
 
+
 StructuredRegionItem = RegionItem
+"""
+@brief 结构化区域项目数据类
+@details 区域数据的结构化版本别名，与原始 RegionItem 相同
+"""
 
 
 if __name__ == '__main__':
