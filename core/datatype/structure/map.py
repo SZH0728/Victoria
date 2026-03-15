3953cfe784d6665e180606eba941d3ff3ce34d33
Sun0728 <15064807+sun0728@user.noreply.gitee.com>
2026-03-15 13:10:05 +0800

    feat(datatype): 添加结构化地图数据类

---
+++ core/datatype/structure/map.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# AUTHOR: Sun
+
+from core.datatype.source.map import MapResource, MapRegion
+
+
+StructuredMapRegion = MapRegion
+StructuredMapResource = MapResource
+
+if __name__ == '__main__':
+    pass
