e32458c4497644c3a8dcf18e9761939cf1d20d95
Sun0728 <15064807+sun0728@user.noreply.gitee.com>
2026-03-15 13:58:02 +0800

    feat(datatype): 完善结构化地图数据类定义

--- core/datatype/structure/map.py
+++ core/datatype/structure/map.py
@@ -1,11 +1,26 @@
 # -*- coding:utf-8 -*-
 # AUTHOR: Sun
 
+"""
+@brief 结构化地图数据模块
+@details 提供地图数据类的结构化版本别名
+"""
+
 from core.datatype.source.map import MapResource, MapRegion
 
 
 StructuredMapRegion = MapRegion
+"""
+@brief 结构化地图区域数据类
+@details 地图区域数据的结构化版本别名，与原始 MapRegion 相同
+"""
+
 StructuredMapResource = MapResource
+"""
+@brief 结构化地图资源数据类
+@details 地图资源数据的结构化版本别名，与原始 MapResource 相同
+"""
+
 
 if __name__ == '__main__':
     pass
